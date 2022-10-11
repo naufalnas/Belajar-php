6178fdba2b5b45568cc730d268e658baceaa6487
naufalnas <115072080+naufalnas@users.noreply.github.com>
2022-10-11 13:22:51 +0700

    Tugas php NaufalNas

--- soal-string.php
+++ soal-string.php
@@ -27,15 +27,18 @@
         $string = "PHP is never old";
         echo "Panjang string: ". strlen($string);
         echo "<br>";
-        echo "Jumlah kata: " . str_word_count($string); 
+        echo "Jumlah kata: ". str_word_count($string); 
         // --------------------------------------- //
 
         
         echo "<h3> Soal No 1</h3>";
 
-        $first_sentence = "Hello PHP!" ; // Panjang string 10, jumlah kata: 2
+        $first_sentence = "Hello PHP!" ;
+        echo "Panjang string: " .strlen($first_sentence) . "," ."Jumlah kata: " .str_word_count($first_sentence). " "."<strong>Dari kata"." ".($first_sentence) . "</strong>";        
+        echo "<br>";
+        // Panjang string 10, jumlah kata: 2
         $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
-        
+        echo "Panjang string: " .strlen($second_sentence) . "," ."Jumlah kata: " .str_word_count($second_sentence). " "."<strong>Dari kata"." ".($second_sentence) . "</strong>";        
         echo "<h3> Soal No 2</h3>";
         /* 
             SOAL NO 2
@@ -48,8 +51,8 @@
         echo "<label>String: </label> \"$string2\" <br>";
         echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
         // Lanjutkan di bawah ini
-        echo "Kata kedua: " ;
-        echo "<br> Kata Ketiga: " ;
+        echo "Kata kedua: " . substr($string2, 2, 4) . "<br>" ;
+        echo "Kata ketiga: " . substr($string2, 6, 7) . "<br>" ;
         echo "<h3> Soal No 3 </h3>";
         /*
             SOAL NO 3
@@ -58,6 +61,9 @@
         $string3 = "PHP is old but Good!";
         echo "String: \"$string3\" "; 
         // OUTPUT : "PHP is old but awesome"
+        echo "<br>";
+        echo "Karakter berubah menjadi: ". str_replace("Good", "awesome", $string3);
+
 
     ?>
 </body>
